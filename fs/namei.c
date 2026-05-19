e790f2dd70d34f51394c4754bfc42c67c00addc9
Flopster101 <nahuelgomez329@gmail.com>
2026-05-19 01:33:15 -0300

    fs: Implement NoMount
    * https://github.com/maxsteeel/nomount/tree/29652753260c6f1cd558df64730d3314c75bf076/kernel
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>
    Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

--- fs/namei.c
+++ fs/namei.c
@@ -135,6 +135,11 @@ extern const struct qstr susfs_fake_qstr_name;
 
 #define EMBEDDED_NAME_MAX	(PATH_MAX - offsetof(struct filename, iname))
 
+#ifdef CONFIG_NOMOUNT
+extern struct filename *nomount_getname_hook(struct filename *name);
+extern int nomount_allow_access(struct inode *inode, int mask);
+#endif
+
 struct filename *
 getname_flags(const char __user *filename, int flags, int *empty)
 {
@@ -210,6 +215,11 @@ getname_flags(const char __user *filename, int flags, int *empty)
 
 	result->uptr = filename;
 	result->aname = NULL;
+#ifdef CONFIG_NOMOUNT
+	if (!IS_ERR(result)) {
+		result = nomount_getname_hook(result);
+	}
+#endif
 	audit_getname(result);
 	return result;
 }
@@ -344,6 +354,12 @@ int generic_permission(struct inode *inode, int mask)
 {
 	int ret;
 
+#ifdef CONFIG_NOMOUNT
+	int nm_perm = nomount_allow_access(inode, mask);
+	if (unlikely(nm_perm < 0)) return nm_perm;
+	if (unlikely(nm_perm > 0)) return 0;
+#endif
+
 	/*
 	 * Do the basic permission checks.
 	 */
@@ -419,6 +435,12 @@ static inline int do_inode_permission(struct vfsmount *mnt, struct inode *inode,
 int __inode_permission2(struct vfsmount *mnt, struct inode *inode, int mask)
 {
 	int retval;
+
+#ifdef CONFIG_NOMOUNT
+	int nm_perm = nomount_allow_access(inode, mask);
+	if (unlikely(nm_perm < 0)) return nm_perm;
+	if (unlikely(nm_perm > 0)) return 0;
+#endif
 
 	if (unlikely(mask & MAY_WRITE)) {
 		/*
--- fs/readdir.c
+++ fs/readdir.c
@@ -23,6 +23,33 @@
 
 #include <linux/uaccess.h>
 
+#ifdef CONFIG_NOMOUNT
+extern void nomount_vfs_inject_dir(struct file *file, struct dir_context *ctx);
+extern bool nomount_should_skip(void);
+extern const loff_t nomount_magic_pos;
+
+#define nomount_handle_iterate_dir(file, ctx, shared, res)               \
+do {                                                                     \
+    loff_t _old_pos = (ctx)->pos;                                        \
+    bool _nm_skip = nomount_should_skip();                               \
+                                                                         \
+    if ((ctx)->pos >= nomount_magic_pos && !_nm_skip) {                  \
+        (res) = 0;                                                       \
+    } else {                                                             \
+        if (shared)                                                      \
+            (res) = (file)->f_op->iterate_shared((file), (ctx));         \
+        else                                                             \
+            (res) = (file)->f_op->iterate((file), (ctx));                \
+    }                                                                    \
+                                                                         \
+    if ((res) >= 0 && !_nm_skip) {                                       \
+        if ((ctx)->pos == _old_pos || (ctx)->pos >= nomount_magic_pos) { \
+            nomount_vfs_inject_dir((file), (ctx));                       \
+        }                                                                \
+    }                                                                    \
+} while (0)
+#endif
+
 #ifdef CONFIG_KSU_SUSFS_SUS_PATH
 #include <linux/susfs_def.h>
 extern bool susfs_is_inode_sus_path(struct inode *inode);
@@ -52,10 +79,14 @@ int iterate_dir(struct file *file, struct dir_context *ctx)
 	res = -ENOENT;
 	if (!IS_DEADDIR(inode)) {
 		ctx->pos = file->f_pos;
+#ifdef CONFIG_NOMOUNT
+		nomount_handle_iterate_dir(file, ctx, shared, res);
+#else
 		if (shared)
 			res = file->f_op->iterate_shared(file, ctx);
 		else
 			res = file->f_op->iterate(file, ctx);
+#endif
 		file->f_pos = ctx->pos;
 		fsnotify_access(file);
 		file_accessed(file);
